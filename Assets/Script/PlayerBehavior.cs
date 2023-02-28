7c3cc6b4ad6213e0b0ce13c5f0ad6dcd2f24e2af
Ruellan Thibaut <thibaut.ruellan@gmail.com>
2023-02-28 11:12:56 +0100

    Player et Dash

---
+++ Assets/Script/PlayerBehavior.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float pullForce;
+
+    public float dashSpeed;
+    public float dashLength;
+    public int dashNumber = 3;
+    private bool isDashing;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotation();
+
+        Dash();
+    }
+
+    public void Rotation()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        Vector3 lookVector = Vector3.forward * ver + -Vector3.left * hor;
+
+        if (hor != 0 && ver != 0)
+        {
+            transform.rotation = Quaternion.LookRotation(lookVector);
+        }
+    }
+
+    public void Dash()
+    {
+        //gravité vers le bas
+        if (!isDashing)
+        {
+            rb.velocity = new Vector3(0, 0, -pullForce);
+        }
+
+        //dash
+        if (Input.GetButtonDown("Dash") && !isDashing && dashNumber != 0)
+        {
+            rb.AddForce(transform.forward * dashSpeed, ForceMode.Impulse);
+            isDashing = true;
+            dashNumber--;
+
+            StartCoroutine(EndDash());
+        }
+    }
+
+    IEnumerator EndDash()
+    {
+        yield return new WaitForSeconds(dashLength);
+        isDashing = false;
+    }
+}
